39305801ec50059ce21c5e62e461621feb28ebd6
ayazkhan-6 <mrjacobian44@gmail.com>
2025-01-11 17:38:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,41 @@
+// let score ="7abc"
+
+
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+
+
+// console.log(valueInNumber)
+
+
+// 33 convert into "33"
+//  33abc covert into NaN
+//  null into 0
+//undefined into NaN
+
+
+
+
+// let isLoggedIn = 1
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+
+
+
+// console.log( booleanisLoggedIn)
+
+//  for boolean
+//  1=> true 0=> false
+// "" =. false agr empty string variable me loge to output false ayegi
+// "ayaz"  => true
+
+
+let a = "33"
+ 
+let b = String(a)
+
+console.log(typeof b)
