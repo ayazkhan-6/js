8af4c809bfdc68506705da44244db8f949302365
ayazkhan-6 <mrjacobian44@gmail.com>
2025-01-12 17:09:48 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+let Name = "ayaz"
+let repoCount = 40
+
+// console.log(Name + repoCount + value);
+
+// console.log(`hello my name is ${Name} and my repo count is${repoCount} `);
+
+const gameName = new String("ayazkhan")
+// console.log(gameName[0]);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf('y'));
+// console.log(gameName.charAt(2));
+
+// const newstring = gameName.substring(0,4)
+// console.log(newstring);
+
+// const anothernewstring = gameName.slice(-7,3)
+// console.log(anothernewstring);
+
+
+// const string = "     khan   "
+// console.log(string);
+// console.log(string.trim());
+
+
+const url = "https://ayaz.com%20rana";
+console.log(url.replace(/%20/g, '-'));
+
+
+
