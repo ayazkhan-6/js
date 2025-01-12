43c9eab02b7e767a21e2e3cc8a4b9423b0c1a564
ayazkhan-6 <mrjacobian44@gmail.com>
2025-01-12 17:49:57 +0000

    nums and maths

---
+++ 01_basics/06_nums-math.js
@@ -0,0 +1,43 @@
+const score = 100
+console.log(score);
+
+
+
+const balance= new Number(760)
+console.log(balance);
+
+console.log(balance.toFixed(3));
+
+const num = 567.324
+console.log(num.toPrecision(3));
+
+
+const lac = 1000000
+console.log(lac.toLocaleString('en-IN'));
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(89.67));
+// console.log(Math.ceil(7.6));
+// console.log(Math.floor(8.9));
+// console.log(Math.min(8,7 , 9 ,90,1));
+// console.log(Math.max(8,7 , 9 ,90,1));
+
+
+const min =10
+ const max =20
+ console.log(Math.floor(Math.random()*(max-min +1))+min)
+
+
+
+
+
+
+
+
+
