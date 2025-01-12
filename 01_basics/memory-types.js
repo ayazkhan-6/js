e073f1ca324af4dc9a15edb94ab231f840d786ec
ayazkhan-6 <mrjacobian44@gmail.com>
2025-01-12 16:18:57 +0000

    memory types

---
+++ 01_basics/memory-types.js
@@ -0,0 +1,36 @@
+//  stack (primitive)      
+
+let myyoutubeName = "ayaz khan yt"
+let anotherName = myyoutubeName
+ anotherName = "Ayaz"
+console.log(myyoutubeName);
+console.log(anotherName);
+
+
+
+
+// heap(non primitive)
+
+
+let userOne ={
+    email:"usergoogle.com"
+    
+}
+
+let userTwo = userOne
+
+userTwo.email = "usergmail.com"
+
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+
+
+
+
+
+
+
