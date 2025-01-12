6e9e642bb4cf202c9a1fc86053df2620c8ef7bab
ayazkhan-6 <mrjacobian44@gmail.com>
2025-01-12 11:13:56 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log("2"> 1);
+
+// console.log(null >0);
+// console.log(null <0);
+// console.log(null >=0);
+
+
+//  === strictly check valye as well as datatype like
+// console.log("2" === 1);
+// it will not execute it
+
+
+
